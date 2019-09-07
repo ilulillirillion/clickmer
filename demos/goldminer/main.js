9a2e716cb7bc9db6b15f4860524c6330cb53106c
Justin Heil <zolvaring@gmail.com>
2019-09-07 15:53:42 -0700

    Mine gold when button is clicked

--- demos/goldminer/main.js
+++ demos/goldminer/main.js
@@ -2,3 +2,8 @@ var gameData = {
   gold: 0,
   goldPerClick: 1
 }
+
+function mineGold() {
+  gameData.gold += gameData.goldPerClick
+  document.getElementById("goldMined").innerHTML = gameData.gold + " Gold Mined"
+}
