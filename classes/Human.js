c09a3410ef20657a35b415ef614db1d9ea6ba534
Justin Heil <zolvaring@gmail.com>
2019-09-27 16:42:20 -0700

    Randomly determine sex

--- classes/Human.js
+++ classes/Human.js
@@ -11,9 +11,19 @@ export default class Human extends Actor {
     //this.statistics.hunger.current = 4320000;
     //this.statistics.hunger.max = 4320000;
 
-    this.sex = 'male';
+    //this.sex = 'male';
+    this.sex = this.randomlyDetermineSex();
   };
 
+  randomlyDetermineSex() {
+    console.debug(`Determining random sex for <${this.class_name}> <${this.uuid}>.`);
+    let sexes = [ 'male', 'female' ];
+    let random_index = Math.floor(Math.random() * sexes.length);
+    console.debug(`Determing <${this.uuid}>'s sex from sexes: <${sexes}> with random index <${random_index}>.`);
+    let sex = sexes[random_index];
+    console.debug(`Returning random sex <${sex}> for <${this.uuid}>.`);
+    return sex;
+  };
 
 
 /*
