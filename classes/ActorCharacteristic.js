f003d6751a2fb2f155ff0e4740f7d96e74801f5a
Justin Heil <zolvaring@gmail.com>
2019-09-30 21:02:22 -0700

    Level up characteristics

--- classes/ActorCharacteristic.js
+++ classes/ActorCharacteristic.js
@@ -5,7 +5,7 @@ import ActorStatistic from '../classes/ActorStatistic.js';
 export default class ActorCharacteristic extends ActorSkillMixin(ActorStatistic) {
   constructor({ name = 'characteristic', actor } = {}) {
     super({ name: name, actor: actor});
-    this.maximum = 1;
+    this.maximum = 100;
     this.current = 1;
     this.minimum = 1;
   };
