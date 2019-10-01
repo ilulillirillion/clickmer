0b7bdee1d72234e528ea4029ca9d6186f01cc8be
Justin Heil <zolvaring@gmail.com>
2019-09-30 18:23:00 -0700

    Add ActorSkills class

---
+++ classes/ActorSkills.js
@@ -0,0 +1,39 @@
+import Thing from '../classes/Thing.js';
+import ActorSkill from '../classes/ActorSkill.js';
+
+
+export default class ActorSkills extends Thing {
+  static skills = [
+    'fighting',
+    'hunting',
+    'survivalism',
+    'tracking'
+  ];
+  constructor() {
+    super();
+
+    this.all = [];
+    for (let skill_name of ActorSkills.skills) {
+      let skill = new ActorSkill({ name: skill_name });
+      this[skill_name] = skill;
+      this.all.push(skill);
+    };
+
+    /*
+    // Fighting.
+    this.fighting = new ActorSkill('fighting');
+
+    // Hunting.
+    this.hunting = new ActorSkill('hunting');
+
+    // Survivalism.
+    this.survivalism = new ActorSkill('survivalism');
+
+    // Tracking.
+    this.tracking = new ActorSkill('tracking');
+    */
+
+  }
+
+  
+}
