d17d41f15b6ca0a4b09a4aa24efbaeed1fc38ed7
Justin Heil <zolvaring@gmail.com>
2019-10-01 13:07:25 -0700

    Add World class

---
+++ classes/World.js
@@ -0,0 +1,12 @@
+import Thing from '../classes/Thing.js';
+
+
+export default class World extends Thing {
+  constructor() {
+    super();
+
+    // Name assignment.
+    this.name = 'world';
+    
+  }
+}
