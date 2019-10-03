c9b1028215f04342433182ce9e48bd36cb3f7c70
Justin Heil <zolvaring@gmail.com>
2019-10-03 10:24:35 -0700

    Reduce logging statements and deprecated code

--- classes/Actor.js
+++ classes/Actor.js
@@ -17,62 +17,56 @@ export default class Actor extends Thing {
         uuid: null, name: null,
       }) {
     super();
-    console.debug(`Instantiating Actor <${this.uuid}>.`);
 
     // Name. If a name is not explicitly given, generate a random one.
     if (!name) {
       name = this.generateRandomName();
-      console.debug(`Generated random name <${name}> for <${this.uuid}>.`);
     };
     this.name = name;
-    console.debug(`<${this.uuid}> name set to <${this.name}>.`);
 
+    // Last status assignment.
     this.last_status = null;
 
+    // Characterstics assignemnt.
     this.characteristics = new ActorCharacteristics({ actor: this });
 
-    // Statistics.
+    // Statistics assignment.
     let statistics = new ActorStatistics(this);
     this.statistics = statistics;
     this.statistics.fill();
 
-    console.debug(`<${this.uuid}> statistics set to <${this.statistics}>.`);
-
+    // Activity assignment.
     this.activity = Actor.getActivity('rest');
 
+    // Skills assignment.
     let skills = new ActorSkills({ actor: this });
     this.skills = skills;
 
+    // Write that a new populant has appeared.
     this.write(`${this.name} has joined the colony!`);
 
   }
 
-
   get active() {
-    console.debug(`Getting <${this.class_name}> <${this.uuid}>'s active state.`);
+    """ Check whether or not the actor is active or 'in play'. """
     let active = true;
     if (this.status.health == 'dead') {
       active = false;
     };
-    console.debug(`Returning <${active}> for <${this.class_name}> <${this.uuid}>'s active state.`);
     return active;
   };
 
 
   get status() {
-    console.debug(`Getting <${this.class_name}> <${this.uuid}>'s status.`);
-
     let status = {};
 
     let old_status = this.last_status;
     status.health = 'healthy';
     // Can't come back from the dead.
     if (old_status != null && old_status.health == 'dead') {
-      console.debug(`Setting <${this.uuid}>'s status to dead because it was dead last time it was checked.`);
       status.health = 'dead'
     };
     if (this.statistics.hunger.current <= 0) {
-      console.debug(`Setting <${this.uuid}>'s status to dead because it's hunger is 0 or lower.`);
       status.health = 'dead';
     };
     old_status = status;
@@ -81,9 +75,7 @@ export default class Actor extends Thing {
   
 
   tick() {
-    console.debug(`Ticking <${this.class_name}> <${this.uuid}>.`);
     super.tick();
-    //this.statistics.hunger.update(-1);
     this.statistics.hunger.current -= 1;
 
     if (!this.sequence) {
@@ -111,10 +103,8 @@ export default class Actor extends Thing {
   };
 
   generateRandomName() {
-    console.log('generating a random name');
     let names = ['bob', 'emily', 'jon', 'ashley', 'justin', 'sara', 'ed'];
     if (this.sex == 'female') {
-      console.debug(`Assigning a female name to <${this.class_name}> <${this.uuid}> because they are female (<${this.sex}>.`);
       names = [ 
           // HP names
           'hermoine',
@@ -229,23 +219,18 @@ export default class Actor extends Thing {
   }
 
   die() {
-    console.debug(`<${this.uuid}> has died.`);
     this.write(`${this.name} has died!`);
-    //this.dead = true;
     this.status = 'dead';
   };
 
 
   get doable_activities() {
-    console.debug(`Getting <${this.uuid}>'s doable activities.`);
     let doable_activities = [];
     for (let activity of Actor.activities) {
-      console.debug(`Checking if <${this.uuid}> can do <${activity.name}> based on <${activity.requirements}>.`);
       let doable = true;
       for (let [skill_name, required_level] of Object.entries(activity.requirements)) {
         if (  !(skill_name in this.skills) || 
               this.skills[skill_name].level < required_level ) {
-          console.debug(`<${this.uuid}> cannot do <${activity.name}> because theyre <${skill_name}> skill does not meet requirement of <${required_level}> (<${this.skills[skill_name]}>).`);
           doable = false;
         };
       };
@@ -256,24 +241,17 @@ export default class Actor extends Thing {
     return doable_activities;
   };
 
-  //levelUp(skill_or_attribute) {
-
-
   getActivity(name) {
     let activity = Actor.getActivity(name);
     return activity;
   };
 
   static getActivity(name) {
-    console.debug(`Getting Actor activity by name <${name}>.`);
     let activity = Actor.activities.find(activity => activity.name === name);
-    console.debug(`Returning Actor activity <${activity.name}>.`);
     return activity;
   };
 
   static activities = [
-    //new IdleActivity()
-    //IdleActivity,
     RestActivity,
     StudyEnvironmentActivity,
     HuntPreyActivity
