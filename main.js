d322d2de001529ff4566ca087e4bbe490b43ff43
Justin Heil <zolvaring@gmail.com>
2019-10-04 21:33:55 -0700

    Test code

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-import Thing from '../classes/Thing.js';
+import Game from '../classes/Game.js';
+import Actor from '../classes/Actor.js';
 
 
-let thing = new Thing();
+let game = new Game();
+
+window.dispatchEvent = function(event_name) {
+  game.dispatchEvent(event_name);
+};
+
+game.registerEvent('test event');
+game.addEventListener('test event', function() {
+  console.debug('test event fired');
+});
+game.dispatchEvent('test event');
+
+
+let actor = new Actor();
