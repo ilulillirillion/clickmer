c44b27a14539c98e43199ec1b2e40158710ade85
Justin Heil <zolvaring@gmail.com>
2019-10-04 21:32:09 -0700

    Add new game class

---
+++ classes/Game.js
@@ -0,0 +1,11 @@
+import Thing from '../classes/Thing.js';
+import ReactorMixin from '../mixins/ReactorMixin.js';
+
+
+export default class Game extends ReactorMixin(Thing) {
+
+  constructor() {
+    super();
+  };
+
+};
