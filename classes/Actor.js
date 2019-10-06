aa0cd76e0a50fcf4cb4fd3b567759eb30ae72310
Justin Heil <zolvaring@gmail.com>
2019-10-06 11:18:38 -0700

    Remove component classes, minimize gets and sets

--- classes/Actor.js
+++ classes/Actor.js
@@ -1,8 +1,7 @@
 import Thing from '../classes/Thing.js';
-import StatisticComponent from '../classes/StatisticComponent.js';
-import SkillComponent from '../classes/SkillComponent.js';
-import CharacteristicComponent from '../classes/CharacteristicComponent.js';
-import TaskComponent from '../classes/TaskComponent.js';
+import ActorStatistic from '../classes/ActorStatistic.js';
+import ActorProficiency from '../classes/ActorProficiency.js';
+import ActorTask from '../classes/ActorTask.js';
 import available_names from '../objects/names.js';
 
 
@@ -57,16 +56,29 @@ export default class Actor extends Thing {
     this.skills = skills;
     */
 
+    /*
+    let characteristics = {
+      vitality: { minimum: 1, maximum: 100, current: 1 }
+    };
+
+    let statistics = {
+      health: { minimum: 0, maximum: 
+    */
+
+
     let characteristics = {};
     //characteristics.vitality = new this.ActorCharacteristic(
-    characteristics.vitality = new CharacteristicComponent(
-        { owner: this, name: 'vitality', current: 10 });
+    //characteristics.vitality = new CharacteristicComponent(
+    //characteristics.vitality = new ActorStatistic(
+    characteristics.vitality = new ActorProficiency(
+        { name: 'vitality', minimum: 1, current: 10 });
     this.characteristics = characteristics;
 
     let statistics = {};
-    //statistics.health = new this.ActorStatistic({ name: 'health' });
-    statistics.health = new StatisticComponent(
-        { owner: this, name: 'health' });
+    statistics.health = new ActorStatistic({ name: 'health' });
+    //statistics.health = new StatisticComponent(
+    //statistics.health = new StatisticComponent(
+    //    { owner: this, name: 'health' });
     Object.defineProperty(statistics.health, 'maximum', {
       get: function() {
         let vitality = self.characteristics.vitality.current;
@@ -74,9 +86,9 @@ export default class Actor extends Thing {
         return maximum;
       }
     });
-    //statistics.hunger = new this.ActorStatistic({ name: 'hunger' });
-    statistics.hunger = new StatisticComponent(
-        { owner: this, name: 'hunger' });
+    statistics.hunger = new ActorStatistic({ name: 'hunger' });
+    //statistics.hunger = new StatisticComponent(
+    //    { owner: this, name: 'hunger' });
     Object.defineProperty(statistics.hunger, 'maximum', {
       get: function() {
         let vitality = self.characteristics.vitality.current;
@@ -84,9 +96,9 @@ export default class Actor extends Thing {
         return maximum;
       }
     });
-    //statistics.stamina = new this.ActorStatistic({ name: 'stamina' });
-    statistics.stamina = new StatisticComponent(
-        { owner: this, name: 'stamina' });
+    statistics.stamina = new ActorStatistic({ name: 'stamina' });
+    //statistics.stamina = new StatisticComponent(
+    //    { owner: this, name: 'stamina' });
     Object.defineProperty(statistics.stamina, 'maximum', {
       get: function() {
         let vitality = self.characteristics.vitality.current;
@@ -97,15 +109,18 @@ export default class Actor extends Thing {
     this.statistics = statistics;
     this.fill_statistics();
 
-    this.task = new TaskComponent(
-        { owner: this, name: 'idle', simple_present_name: 'idling' });
+    //this.task = new TaskComponent(
+    //    { owner: this, name: 'idle', simple_present_name: 'idling' });
+    this.task = new ActorTask(
+        { name: 'idle', simple_present_name: 'idling' });
 
   };
 
   tick() {
     super.tick();
-    this.task.tick();
+    //this.task.tick();
     //this.statistics.hunger.current -= 1;
+    this.task.apply_tick_effects({ participant: this });
   }; 
 
   fill_statistics() {
@@ -113,6 +128,33 @@ export default class Actor extends Thing {
       statistic.fill();
     };
   }; 
+
+  getProficiency(proficiency_name) {
+    if (proficiency_name in this.characteristics) {
+      let proficiency = this.characteristics[proficiency];
+    } else {
+      let proficiency_name = this.skills[proficiency];
+    };
+    return proficiency;
+  };
+
+  gainExperience(proficiency_name, experience_gain) {
+    proficiency = this.getProficiency(proficiency_name);
+    proficiency._experience += experience_gain;
+    while (proficiency._experience >= profiency._experience_to_next_level) {
+      this.levelUp(proficiency);
+    };
+  };
+  
+  levelUp(proficiency_name) {
+    let proficiency = this.getProficiency(proficiency_name);
+    let experience_requirements_gain = Math.ceil((
+        proficiency._experience_to_next_level + 
+        (proficiency._experience_to_next_level * 0.10)
+    ));
+    proficiency._experience_to_next_level = experience_requirements_gain;
+    proficiency.current += 1;
+  };
 
   getRandomName() {
     let names = available_names.male_names;
