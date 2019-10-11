2d62a091a1ef62628e210b48b8e700e8e6b5371c
Justin Heil <zolvaring@gmail.com>
2019-10-10 19:28:03 -0700

    Add login system from tutorial

---
+++ README.md
@@ -0,0 +1,5 @@
+Great things I've learned from:
+  - https://codeshack.io/basic-login-system-nodejs-express-mysql/
+    Currently using this to figure out a basic authentication system.
+  - https://hackernoon.com/how-to-build-a-multiplayer-browser-game-4a793818c29b
+    Used this as a launching point to learn how to build an express server and app.
--- server.js
+++ server.js
@@ -3,11 +3,21 @@
 
 //const winston = require('winston');
 //const logger = require('./source/classes/Logger.js');
-const logger = require('./source/misc/winston_logger.js');
-const express = require('express');
-const http = require('http');
-const path = require('path');
-const socketIO = require('socket.io');
+var logger = require('./source/misc/winston_logger.js');
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var socketIO = require('socket.io');
+var mysql = require('mysql');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : process.env.CLICKER_MYSQL_PASSWORD,
+	database : 'nodelogin'
+});
 
 //winston.log('info', 'Clickmer started');
 logger.info('Clickmer started');
@@ -17,12 +27,64 @@ var app = express();
 app.set('port', 5000);
 // Serve client files in the client directory
 //app.use(express.static(path.join(__dirname, './client/view')));
+
+app.use(session({
+  secret: process.env.CLICKER_SESSION_SECRET,
+  //secret: 'test',
+  resave: true,
+  save_uninitialized: true
+}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
 app.use(express.static(path.join(__dirname, './client/')));
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname, 'client/view/index.html'));
   //response.sendFile(path.join(__dirname, 'client/view/main.js'));
 });
+app.get('/login', function(request, response) {
+  response.sendFile(path.join(__dirname, 'client/view/login.html'));
+});
 logger.info('App created.');
+
+
+app.post('/auth', function(request, response) {
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+      logger.info(arguments);
+      logger.info(`${error}, ${results}, ${fields}`);
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+
+app.get('/home', function(request, response) {
+	if (request.session.loggedin) {
+		response.send('Welcome back, ' + request.session.username + '!');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
+
+
+
 
 var server = http.Server(app);
 server.listen(5000, function() {
