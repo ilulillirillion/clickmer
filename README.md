14e1be3436cea1e14b554b87fc9551fd7b9e4228
Justin Heil <zolvaring@gmail.com>
2019-10-19 17:32:33 -0700

    Switch from Angular to React

--- README.md
+++ README.md
@@ -7,6 +7,11 @@ A simple Javascript game with Node and Angular.
 ## Commit journal
 ---
 
+### Commit 278
+#### Switch from Angular to React
+
+After reading about React it seemed like a purer solution for just updating view (I want to handle model) than using Angular, so I have gotten a small React class going in the test page showing the player object and name on login.
+
 ### Commit 277
 #### Fix server responding before finishing player
 
--- client/main.js
+++ client/main.js
@@ -1,16 +1,74 @@
 console.info('Running main.js');
 
 
-var app = angular.module('Clickmer', []);
-app.controller('Controller', function($scope) {
+const name = 'test';
+//var player = { 'name': 'dummyplayer' };
+//const element = <p>hello {name}</p>;
 
-  let socket = io();
+class PlayerInfo extends React.Component {
+  constructor(
+      { player, props, context } = 
+      {} ) {
+    console.debug('Contstructing PlayerInfo with arguments', arguments);
+    super(props, context);
+    
+    this.player = player;
 
-  socket.emit('connect_player', null, function(response) {
-    console.debug('connect_player response: ', response);
+  }
+    
+  render() {
+    return (
+      React.createElement('div', {},
+        React.createElement('p', {}, `player: ${this.player}`),
+        React.createElement('p', {}, `player name: ${this.player.name}`)
+      )
+    );
+  };
+};
+
+/*
+ReactDOM.render(
+  //element,
+  //document.getElementById('root')
+  React.createElement('p', {}, `hello ${name}`),
+  React.createElement('p', {}, `player: ${player}`),
+  //React.createElement('p', {}, `player name: ${player.name}`),
+  document.getElementById('react_test')
+);
+*/
+
+
+
+//var app = angular.module('Clickmer', []);
+//app.controller('Controller', function($scope) {
+
+let socket = io();
+
+async function buildPlayerInfo() {
+
+  //var player = { name: 'test' };
+  let player = await new Promise((resolve, reject) => {
+    try {
+      socket.emit('connect_player', null, function(response) {
+        console.debug('connect_player response: ', response);
+        resolve(response);
+      });
+    } catch(error) {
+      console.error(error);
+    };
   });
 
-});
 
+  //let test = new Test();
+  let player_info = new PlayerInfo({ player: player });
+  console.debug('player info', player_info); 
+  ReactDOM.render(
+    player_info.render(),
+    document.getElementById('react_test')
+  );
+
+};
+
+buildPlayerInfo();
 
 console.info('Finished running main.js');
